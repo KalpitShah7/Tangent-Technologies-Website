3d135e541201ceb89123807c11517dd47ac1d7d1
KalpitShah7 <kalpit.cba1550@ict.gnu.ac.in>
2018-01-18 19:12:50 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,177 @@
+
+ /* jQuery Preloader
+  -----------------------------------------------*/
+$(window).load(function(){
+    $('.preloader').fadeOut(1000); // set duration in brackets    
+});
+
+
+$(document).ready(function() {
+
+  /* Hide mobile menu after clicking on a link
+    -----------------------------------------------*/
+    $('.navbar-collapse a').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+
+
+  /* jQuery to collapse the navbar on scroll
+    -----------------------------------------------*/
+  $(window).scroll(function() {
+      if ($(".navbar").offset().top > 50) {
+          $(".navbar-fixed-top").addClass("top-nav-collapse");
+      } else {
+          $(".navbar-fixed-top").removeClass("top-nav-collapse");
+      }
+  });
+
+
+  /* BxSlider
+    -----------------------------------------------*/
+  (function (window, $) {
+  'use strict';
+
+  // Cache document for fast access.
+  var document = window.document;
+
+  function mainSlider() {
+    $('.bxslider').bxSlider({
+		auto: true,
+      pagerCustom: '#bx-pager',
+      mode: 'fade',
+      nextText: '',
+      prevText: ''
+    });
+  }
+
+  mainSlider();
+
+  var $links = $(".bx-wrapper .bx-controls-direction a, #bx-pager a");
+  $links.click(function(){
+     $(".slider-caption").removeClass('animated fadeInLeft');
+     $(".slider-caption").addClass('animated fadeInLeft');
+  });
+
+  $(".bx-controls").addClass('container');
+  $(".bx-next").addClass('fa fa-angle-right');
+  $(".bx-prev").addClass('fa fa-angle-left');
+
+
+  })(window, jQuery);
+
+
+   /* Owl Carousel
+    -----------------------------------------------*/
+  $(document).ready(function() {
+    $("#owl-work").owlCarousel({
+      autoPlay: 3000,
+      items : 3,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3],
+    });
+  });
+  
+
+  /* Parallax section
+    -----------------------------------------------*/
+  function initParallax() {
+    $('#work').parallax("100%", 0.3);
+    $('#about').parallax("100%", 0.2);
+    $('#team').parallax("100%", 0.3);
+    $('#portfolio').parallax("100%", 0.1);
+    $('#plan').parallax("100%", 0.3);
+    $('#contact').parallax("100%", 0.2);
+  }
+  initParallax();
+
+
+  /* Nivo lightbox
+    -----------------------------------------------*/
+  $('#portfolio .col-md-4 a').nivoLightbox({
+        effect: 'fadeScale',
+    });
+
+
+  /* wow
+  -------------------------------*/
+  new WOW({ mobile: false }).init();
+
+  });
+
+/* Navigation
+  -----------------------------------------------*/
+  (function ($) {
+
+	new WOW().init();
+	
+	$(window).load(function(){
+      $("#navigation").sticky({ topSpacing: 0 });
+    });
+
+	jQuery(window).load(function() { 
+		jQuery("#preloader").delay(100).fadeOut("slow");
+		jQuery("#load").delay(100).fadeOut("slow");
+	});
+
+
+	//jQuery for page scrolling feature - requires jQuery Easing plugin
+	$(function() {
+		$('.navbar-nav li a').bind('click', function(event) {
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top
+			}, 1500, 'easeInOutExpo');
+			event.preventDefault();
+		});
+		$('.page-scroll a').bind('click', function(event) {
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top
+			}, 1500, 'easeInOutExpo');
+			event.preventDefault();
+		});
+	});
+	
+	//owl carousel
+	$('#owl-works').owlCarousel({
+            items : 4,
+            itemsDesktop : [1199,5],
+            itemsDesktopSmall : [980,5],
+            itemsTablet: [768,5],
+            itemsTabletSmall: [550,2],
+            itemsMobile : [480,2],
+        });
+	
+	//nivo lightbox
+	$('.owl-carousel .item a').nivoLightbox({
+		effect: 'fadeScale',                             // The effect to use when showing the lightbox
+		theme: 'default',                           // The lightbox theme to use
+		keyboardNav: true,                          // Enable/Disable keyboard navigation (left/right/escape)
+		clickOverlayToClose: true,                  // If false clicking the "close" button will be the only way to close the lightbox
+		onInit: function(){},                       // Callback when lightbox has loaded
+		beforeShowLightbox: function(){},           // Callback before the lightbox is shown
+		afterShowLightbox: function(lightbox){},    // Callback after the lightbox is shown
+		beforeHideLightbox: function(){},           // Callback before the lightbox is hidden
+		afterHideLightbox: function(){},            // Callback after the lightbox is hidden
+		onPrev: function(element){},                // Callback when the lightbox gallery goes to previous item
+		onNext: function(element){},                // Callback when the lightbox gallery goes to next item
+		errorMessage: 'The requested content cannot be loaded. Please try again later.' // Error message when content can't be loaded
+	});
+	
+	
+	//parallax
+        if ($('.parallax').length)
+        {
+			$(window).stellar({
+				responsive:true,
+                scrollProperty: 'scroll',
+                parallaxElements: false,
+                horizontalScrolling: false,
+                horizontalOffset: 0,
+                verticalOffset: 0
+            });
+
+        }
+
+})(jQuery);
+
